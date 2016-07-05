0d20dc9ac36537434f6511b069c1ae67818e9754
thetuningspoon <mike@thetuningspoon.com>
2016-07-05 15:32:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# SI Tabs - Simple, progressive jQuery Tab Switcher
+Developed by Mike Spooner (thetuningspoon) for Solution Innovators
+
+SI Tabs is a lightweight, accessible jQuery tab switcher that makes use of progressive enhancement and degrades gracefully. If JS is turned off, all tabbed sections will be visible and clicking on a tab will take you to that section of the page (like a traditional internal anchor). Using a hash in the url will take you directly to the tab with the corresponding ID on page load.
+
+## Usage
+
+1. Include the si-tabs.js file in the head section of your page
+2. Add the ".tab" class to your tab links.
+3. Add the ".tabbed" class to the corresponding tabbed sections in the body of the page that you wish to show/hide using the tabs. Give each tabbed section a unique ID.
+4. Use the ID of the section you want to point to in the tab link's href, i.e. href="#myTab"
+5. Add the ".tabbed_default" class on the section you wish to use as the default tab.
+
+
+## Example usage
+
+```html
+<ul>
+	<li><a class="tab" href="#section1">Tab 1</a></li>
+	<li><a class="tab" href="#section2">Tab 2</a></li>
+</ul>
+
+<section class="tabbed" id="section1">
+	I am the first tabbed section.
+</section> 
+<section class="tabbed" id="section2">
+	I am the second tabbed section.
+</section> 
+```
---
+++ si-tabs.js
@@ -0,0 +1,50 @@
+/**
+ * SI Tabs - Simple, progressive jQuery Tab Switcher
+ * Developed by Mike Spooner (thetuningspoon) for Solution Innovators
+ *
+ */
+
+$(document).ready(function() {
+		// Config - Feel free to change these classes to suit your naming conventions
+		var tabClass = 'tab';
+		var tabbedClass = 'tabbed';
+		var defaultTabClass = 'tabbed_default';
+
+    var hash = window.location.hash.substring(1); // Get the anchor hash from the url
+    var $tabbedSections = $('.'+tabbedClass);
+    
+    if(hash)
+        switchTab(hash);
+    else
+        switchTab($('.'+defaultTabClass).attr('id'));
+
+    $('.'+tabClass+'[href*=#]').click(function(e) {
+        var $this = $(this);
+        var tabName = $this.attr('href').substring(1);
+
+        if(tabName == window.location.hash) return; // Stop if we're already on the tab
+
+        e.preventDefault(); // Prevent the default action of jumping to the ID
+
+        // Manually add the hash to the URL since preventDefault stops it from getting added
+        if(history.pushState) {
+            history.pushState(null, null, '#'+tabName);
+        }
+        else {
+            window.location.hash = tabName;
+        }
+
+        switchTab(tabName);
+    });
+
+    /**
+     * @param name Name of the tab to switch to
+     */
+    function switchTab(name) {
+        $tabbedSections.hide(); // Hide all tabs
+        $('#'+ name).show(); // Show selected tab
+        $(this).trigger('tabSwitched');
+
+        $(window).trigger('load resize'); // Makes sure to trigger refresh of any js that responds to the window resize event to calculate layout (i.e. columnizer, matchheight)
+    }
+});
